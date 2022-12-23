e40ff84a2e2dbd1401960a2cd58f7746141e0a32
Kevin Krueger <kevin.krueger@redis.com>
2022-12-23 15:02:14 -0700

    Implete delete code in add_batch

--- dynamodb-composite-key/dynamodb-composite-key.py
+++ dynamodb-composite-key/dynamodb-composite-key.py
@@ -29,6 +29,7 @@ def _(parser):
     parser.add_argument("--jumbo_frequency", type=int, env_var="RED_LOCUST_JUMBO_FREQUENCY", default=50, help="Frequency of jumbo add logic")
     parser.add_argument("--jumbo_initial_exclude", type=int, env_var="RED_LOCUST_JUMBO_INITIAL_EXCLUDE", default=100, help="Number of initial keys to exclude from jumbo logic")
     parser.add_argument("--jumbo_size", type=str, env_var="RED_LOCUST_JUMBO_SIZE", default="25,25,50,100,1000", help="Array representing the extra members for jumbo adds")
+    parser.add_argument("--local_mode", type=str, env_var="RED_LOCUST_LOCAL_MODE", default="Y", help="Use DynamoDB Local Mode")
     parser.add_argument("--version_display", type=str, env_var="RED_VERSION_DISPLAY", default="0.2", help="Just used to show locust file version in UI")
 
 class DynamoDbDataLayer():
@@ -66,7 +67,7 @@ class DynamoDbDataLayer():
             "request_type": request_type,
             "name": name,
             "start_time": start_time,
-            "response_time": (time.perf_counter() - start_time) * 1000 * 1000,
+            "response_time": (end_time - start_time) * 1000,
             "response_length": response_length,
             "response": response,
             "context": {},
@@ -123,7 +124,7 @@ class DynamoDbDataLayer():
 
         transaction_ids = [''.join(random.choices(string.ascii_uppercase + string.digits, k=random.randint(self.environment.parsed_options.value_min_chars, self.environment.parsed_options.value_max_chars)))]
 
-        orig_keyint = (keyint - self.environment.parsed_options.zipf_offset ) * self.environment.parsed_options.zipf_direction
+        orig_keyint = (keyint - self.environment.parsed_options.zipf_offset ) * self.environment.parsed_options.zipf_direction    
         if ((orig_keyint > self.environment.parsed_options.jumbo_initial_exclude)  and (keyint % self.environment.parsed_options.jumbo_frequency == 0) ):
              baseRequestName = "add_jumbo"
              count = int(random.choice(self.environment.parsed_options.jumbo_size.split(',')))
@@ -196,15 +197,16 @@ class DynamoDbDataLayer():
         table = dynamoClient.Table(self.environment.parsed_options.table_name)
 
         # Build keys and member logic for use in later commands
-        baseRequestName = "add_batch"                
-        keyname_and_members_list = []        
+        baseRequestName = "add_batch"          
+        keyname_and_members_list = []
+        transtime = Decimal(time.time())        
 
         for i in range(self.environment.parsed_options.pipeline_size-1):
             keyint = self.get_key_int()
             transaction_ids = [''.join(random.choices(string.ascii_uppercase + string.digits, k=random.randint(self.environment.parsed_options.value_min_chars, self.environment.parsed_options.value_max_chars)))]
 
-            orig_keyint = (keyint * self.environment.parsed_options.zipf_direction) - self.environment.parsed_options.zipf_offset
-            if ((orig_keyint > self.environment.parsed_options.jumbo_initial_exclude)  and (keyint % self.environment.parsed_options.jumbo_frequency == 0) ):
+            orig_keyint = (keyint - self.environment.parsed_options.zipf_offset ) * self.environment.parsed_options.zipf_direction            
+            if ((orig_keyint > self.environment.parsed_options.jumbo_initial_exclude)  and (keyint % self.environment.parsed_options.jumbo_frequency == 0)):         
                 count = int(random.choice(self.environment.parsed_options.jumbo_size.split(',')))
                 for i in range(count):
                     transaction_ids.append(''.join(str(i)).join(random.choices(string.ascii_uppercase + string.digits, k=random.randint(self.environment.parsed_options.value_min_chars, self.environment.parsed_options.value_max_chars))))
@@ -217,8 +219,7 @@ class DynamoDbDataLayer():
             with table.batch_writer() as batch:
                 for i in keyname_and_members_list:                    
                     for transaction_id in i[1]:
-                        transtime = Decimal(time.time())
-                        myResponse = batch.put_item(Item={"Id":i[0],"EventDate":transtime, "TransactionId":transaction_id})
+                        myResponse = batch.put_item(Item={"Id":i[0],"EventDate":Decimal(time.time()), "TransactionId":transaction_id})
 
         except Exception as e:
             myException = e
@@ -232,29 +233,44 @@ class DynamoDbDataLayer():
             response = myResponse,
             exception = myException)
 
+        # Delete old transactions using a batch
+        myResponse = None
+        myException = None
+        trans_start_time = time.perf_counter()
 
-        #TODO: Implement delete in a batch; use delete code in add as a start
+        last_evaluated_key = None
+        items_to_delete = []
+        
+        try:
+            for i in keyname_and_members_list:
+                while True:
+                    # better way to do this conditional call?
+                    if last_evaluated_key:
+                        myResponse = table.query(Select='SPECIFIC_ATTRIBUTES', ProjectionExpression='Id,EventDate', ExclusiveStartKey=last_evaluated_key,
+                            KeyConditionExpression=Key('Id').eq(i[0]) & Key('EventDate').between(0, transtime - self.environment.parsed_options.zrem_seconds))
+                    else:
+                        myResponse = table.query(Select='SPECIFIC_ATTRIBUTES', ProjectionExpression='Id,EventDate',
+                            KeyConditionExpression=Key('Id').eq(i[0]) & Key('EventDate').between(0, transtime - self.environment.parsed_options.zrem_seconds))                                    
+                    items_to_delete.extend(myResponse['Items'])
+                    if not "LastEvaluatedKey" in myResponse:
+                        break                
+                    last_evaluated_key = myResponse['LastEvaluatedKey']
+                        
+            with table.batch_writer(overwrite_by_pkeys=['Id', 'EventDate']) as batch:
+                for i in items_to_delete:                    
+                    myResponse = batch.delete_item(Key={"Id":i['Id'],"EventDate":i['EventDate']})                            
 
-        # Active-active zrem section
-        # myResponse = None
-        # myException = None
-        # trans_start_time = time.perf_counter()
-        # try:
-        #     myResponse = localRedis.zremrangebyscore( \
-        #         ''.join((self.environment.parsed_options.key_name_prefix, str(keyint).zfill(self.environment.parsed_options.key_name_length))), \
-        #         0, transtime - self.environment.parsed_options.zrem_seconds)
-        # except Exception as e:
-        #     myException = e
+        except Exception as e:
+            myException = e
 
-        # self.record_request_meta(
-        #     request_type = "aa",
-        #     name = "zrem",
-        #     start_time = trans_start_time,
-        #     end_time = time.perf_counter(),
-        #     response_length = 0,
-        #     response = myResponse,
-        #     exception = myException)
-    
+        self.record_request_meta(
+            request_type = "",
+            name = "delete_batch",
+            start_time = trans_start_time,
+            end_time = time.perf_counter(),
+            response_length = 0,
+            response = myResponse,
+            exception = myException)
 
 class DynamoDbUser(User):
     """
@@ -299,12 +315,12 @@ def on_test_start(environment, **kwargs):
     else:        
         logging.info("Locust worker or stand-alone node test start")
 
-        #TODO: need TLS?
-        #myTls = (environment.parsed_options.tls == "Y")            
-
-        #TODO: Make local mode configurable
-        endpoint_url = 'http://localhost:8000'
-        myDynamoDb = boto3.resource('dynamodb', endpoint_url=endpoint_url)
+        local_mode = (environment.parsed_options.local_mode == "Y")
+        if local_mode:
+            endpoint_url = 'http://localhost:8000'
+            myDynamoDb = boto3.resource('dynamodb', endpoint_url=endpoint_url)
+        else:
+            myDynamoDb = boto3.resource('dynamodb')
 
         try:
             myDynamoDb.create_table(TableName=environment.parsed_options.table_name, 
